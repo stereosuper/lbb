3babecb6dbe663b4c5a682a6751e219916ec2fde
Elisabeth Hamel <elisabeth@stereosuper.fr>
2015-10-20 16:23:15 +0200

    Menu avec submenu

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,29 @@
 </header>
 
 <nav role='navigation' id='main-menu' class='menu'>
+	<a class='logo' href='./'>la Belle Boîte</a>
 	<div>
-		<a class='logo' href='./'>la Belle Boîte</a>
+		
 		<div>
 			<ul>
 				<li class='actif'><a href='./'>Accueil</a></li>
-				<li><a href='#'>la Belle Boîte</a></li>
-				<li><a href='#'>Pour Vous</a></li>
+				<li>
+					<a href='#'>la Belle Boîte</a>
+					<ul class='submenu'>
+						<li><a href='#'>pourquoi</a></li>
+						<li><a href='#'>pour qui ?</a></li>
+						<li><a href='#'>l'équipe</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href='#'>Pour Vous</a>
+					<ul class='submenu'>
+						<li><a href='#'>prestations</a></li>
+						<li><a href='#'>ateliers</a></li>
+						<li><a href='#'>interventions</a></li>
+						<li><a href='#'>références</a></li>
+					</ul>
+				</li>
 				<li><a href='#'>Blog</a></li>
 				<li><a href='#'>Contact</a></li>
 			</ul>
